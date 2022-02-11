fe5cb1664a0f863f27d696b72140c22b8247912d
MAC <user@ORC-1.local>
2022-02-11 18:49:36 +0200

    document model Test and model implemented

--- watermark/watermark/core/models.py
+++ watermark/watermark/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    ticket_id=models.CharField(max_length=255, unique=True)
+    content = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    topic = models.CharField(max_length=255, blank=True)
+    author = models.CharField(max_length=255)
+    water_mark = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
--- watermark/watermark/core/tests/test_models.py
+++ watermark/watermark/core/tests/test_models.py
@@ -1,1 +1,16 @@
 from django.test import TestCase
+
+from core import models
+
+class ModelTest(TestCase):
+    def test_document_str(self):
+        """Test the document string representation"""
+        doc = models.Document.objects.create(
+            ticket_id='123944-2399403-11s',
+            content='book',
+            title='The Dark Code',
+            author='Bruce Wayne',
+            topic='Science',
+            water_mark=True
+        )
+        self.assertEqual(str(doc), doc.title)
