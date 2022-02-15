f20a3806ff91c9b1b7706921d3b02a85cc8d72e9
MAC <user@ORC-1.local>
2022-02-15 17:31:53 +0200

    Add missing doc string

--- watermark/watermark/wm/watermarker.py
+++ watermark/watermark/wm/watermarker.py
@@ -1,7 +1,7 @@
 from utils.random_id_gen import generate_random_alphanum
 
 async def watermarkdoc(serializer):
-
+    """Water Mark and persist data to DataBase"""
     try:
         serializer.save(ticket_id=generate_random_alphanum(23), water_mark=True)
     except Exception as e:
