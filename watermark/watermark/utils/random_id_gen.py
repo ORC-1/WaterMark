a3292004bda62018f4460d07eddfb9b22a50e701
MAC <user@ORC-1.local>
2022-02-15 17:35:53 +0200

    format random gen fuction

--- watermark/watermark/utils/random_id_gen.py
+++ watermark/watermark/utils/random_id_gen.py
@@ -3,5 +3,7 @@ import string
 
 
 def generate_random_alphanum(lenght):
-    return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in
-                   range(lenght))
+    """Generate random alpha numeric keys using specified lenght"""
+    return ''.join(random.choice(string.ascii_uppercase +
+                    string.ascii_lowercase + string.digits) for _ in
+                    range(lenght))
